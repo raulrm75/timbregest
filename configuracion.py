60da188a1456d84cb4664c5ea6ef468ab147e7fb
Raúl Reina <raulrm75@gmail.com>
2014-10-10 08:42:32 +0200

    Añadido soporte para días libres

--- configuracion.py
+++ configuracion.py
@@ -3,6 +3,8 @@ import os.path
 hora_apagado = '14:35:00'
 raiz_audio = '/home/raul/Escritorio/Timbres'
 raiz_recursos = os.path.join(os.path.dirname(__file__), 'Recursos')
-
+dias_libres = (
+    '13/10/2014',
+    )
 if __name__ == '__main__':
     print(raiz_recursos)
